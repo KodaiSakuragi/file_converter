45c5c6f51882095a3470f5cd7e742d3d42b889f5
櫻木嵩大 <k.sakuragi@koska.jp>
2020-09-04 16:47:17 +0900

    元の拡張子を変更できるようにした

--- file_converter.py
+++ file_converter.py
@@ -4,28 +4,34 @@ import glob
 import os
 import configparser
 
-
+# config設定
 config = configparser.ConfigParser()
 config.read('config.ini', encoding='utf-8')
 delete_file = config['common']['delete_file']
-wav_folder = 'wav'
-avi_folder = 'avi'
+music_extension = config['common']['music_extension']
+video_extension = config['common']['video_extension']
 bps = config['common']['bps']
-wav_data = glob.glob(wav_folder + '/*.wav')
-avi_data = glob.glob(avi_folder + '/*.avi')
+
+# 各フォルダ指定
+music_folder = 'music'
+video_folder = 'video'
 output_folder = 'output'
 
+# configで設定した拡張子のファイルのみを抽出
+music_data = glob.glob(music_folder + '/*.' + music_extension)
+video_data = glob.glob(video_folder + '/*.' + video_extension)
+
 # wavのファイル変換
-if os.listdir(wav_folder) != '[]':
-    for data in wav_data:
+if os.listdir(music_folder) != '[]':
+    for data in music_data:
         print('start:' + os.path.splitext(os.path.basename(data))[0])
         subprocess.run(['ffmpeg', '-i', data, '-vn', '-ac', '2', '-ar', '44100', '-ab', bps + 'k', '-acodec', 'libmp3lame', '-f', 'mp3', output_folder + '/' + os.path.splitext(os.path.basename(data))[0] + '.mp3'])
         if delete_file == '0':
-                os.remove(data)
+            os.remove(data)
 
 # aviのファイル変換
-if os.listdir(avi_folder) != '[]':
-    for data in avi_data:
+if os.listdir(video_folder) != '[]':
+    for data in video_data:
         print('start:' + os.path.splitext(os.path.basename(data))[0])
         subprocess.run(['ffmpeg', '-i', data, output_folder + '/' + os.path.splitext(os.path.basename(data))[0] + '.mp4'])
         if delete_file == '0':
